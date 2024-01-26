bbf7f359ed6fe65cf5a8edf4b7f6ef44826418dc
nikhilcad <nikhilcaddilac@gmail.com>
2024-01-26 22:26:33 +0530

    profilepic update

--- src/components/Sidebar/ProfileLink.jsx
+++ src/components/Sidebar/ProfileLink.jsx
@@ -23,7 +23,7 @@ const ProfileLink = () => {
 				w={{ base: 10, md: "full" }}
 				justifyContent={{ base: "center", md: "flex-start" }}
 			>
-				<Avatar size={"sm"} src={"authUser?.profilePicURL" || ""} />
+				<Avatar size={"sm"} src={"public/profilepic.jpg" || ""} />
 				<Box display={{ base: "none", md: "block" }}>Profile</Box>
 			</Link>
 		</Tooltip>
