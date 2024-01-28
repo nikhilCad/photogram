729b084cbf255aa963eaf08dbd4116817008cd40
nikhilcad <nikhilcaddilac@gmail.com>
2024-01-28 17:27:49 +0530

    firebase witrh hidden api keys

--- README.md
+++ README.md
@@ -10,4 +10,13 @@ Firebase - backend
 Zustand - Global State management for React
 React-firebase-hooks - React for firebase
 
+You must add a .env file at the root of the project with the following variables from your own firebase account
+FIREBASE_APIKEY,
+FIREBASE_AUTHDOMAIN,
+FIREBASE_PROJECTID,
+FIREBASE_STORAGEBUCKET,
+FIREBASE_MESSAGINGSENDERID,
+FIREBASE_APPID,
+FIREBASE_MEASUREMENTID
+
 at 2:46:55
---
+++ src/firebase/firebase.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+//For Environment variables
+require('dotenv').config()
+
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
+
+export { app, auth, firestore, storage };
