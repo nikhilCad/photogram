2149c2d12f8a5568e2b45e7df65d36e2f53306eb
nikhilcad <nikhilcaddilac@gmail.com>
2024-01-30 23:53:00 +0530

    some comments

--- src/components/AuthForm/GoogleAuth.jsx
+++ src/components/AuthForm/GoogleAuth.jsx
@@ -6,10 +6,8 @@ import useAuthStore from "../../store/authStore";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 
 const GoogleAuth = ({ prefix }) => {
-
-	//From react-firebase-hooks docs
-	//can use in both sign in and login
-	const [signInWithGoogle, error] = useSignInWithGoogle(auth);
+	//from react-firebase-hooks
+	const [signInWithGoogle, , , error] = useSignInWithGoogle(auth);
 	const showToast = useShowToast();
 	const loginUser = useAuthStore((state) => state.login);
 
@@ -21,27 +19,24 @@ const GoogleAuth = ({ prefix }) => {
 				return;
 			}
 
-			//check if user exists from the database
+			//check if user exists in the db
 			const userRef = doc(firestore, "users", newUser.user.uid);
 			const userSnap = await getDoc(userRef);
 
 			if (userSnap.exists()) {
-				// login
+				// login if it exists
 				const userDoc = userSnap.data();
 				localStorage.setItem("user-info", JSON.stringify(userDoc));
 				loginUser(userDoc);
 			} else {
-				// signup
+				// signup if it doesnt exists
+				//code mostly taken from the useSignUo hook
 				const userDoc = {
 					uid: newUser.user.uid,
 					email: newUser.user.email,
-					//just create the username from the email lol
-					//how to make sure this is unique???
-					//note that uid is always unique
 					username: newUser.user.email.split("@")[0],
 					fullName: newUser.user.displayName,
 					bio: "",
-					//photourl is inbuilt
 					profilePicURL: newUser.user.photoURL,
 					followers: [],
 					following: [],
@@ -49,6 +44,7 @@ const GoogleAuth = ({ prefix }) => {
 					createdAt: Date.now(),
 				};
 				await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
+				//set localstorage so dont have to login each time
 				localStorage.setItem("user-info", JSON.stringify(userDoc));
 				loginUser(userDoc);
 			}
@@ -59,7 +55,6 @@ const GoogleAuth = ({ prefix }) => {
 
 	return (
 		<Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"} onClick={handleGoogleAuth}>
-        {/* <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"}> */}
 			<Image src='/google.png' w={5} alt='Google logo' />
 			<Text mx='2' color={"blue.500"}>
 				{prefix} with Google
