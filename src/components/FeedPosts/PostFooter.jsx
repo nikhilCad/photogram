3d74410c38a28a58e592490ca26de96b22867bc9
nikhilcad <nikhilcaddilac@gmail.com>
2024-02-03 21:51:24 +0530

    Posts can be liked now

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -3,16 +3,17 @@ import { useRef, useState } from "react";
 import { CommentLogo, NotificationsLogo, UnlikeLogo } from "/src/assests/constants.jsx";
 import usePostComment from "../../hooks/usePostComment";
 import useAuthStore from "../../store/authStore";
-// import useLikePost from "../../hooks/useLikePost";
-// import { timeAgo } from "../../utils/timeAgo";
+import useLikePost from "../../hooks/useLikePost";
+import { timeAgo } from "../../utils/timeAgo";
 // import CommentsModal from "../Modals/CommentsModal";
 
 const PostFooter = ({ post, isProfilePage, creatorProfile }) => {
 	const { isCommenting, handlePostComment } = usePostComment();
 	const [comment, setComment] = useState("");
 	const authUser = useAuthStore((state) => state.user);
+	//focus comment input box when clicking comment button
 	const commentRef = useRef(null);
-	// const { handleLikePost, isLiked, likes } = useLikePost(post);
+	const { handleLikePost, isLiked, likes } = useLikePost(post);
 	const { isOpen, onOpen, onClose } = useDisclosure();
 
 	const handleSubmitComment = async () => {
@@ -23,9 +24,9 @@ const PostFooter = ({ post, isProfilePage, creatorProfile }) => {
 	return (
 		<Box mb={10} marginTop={"auto"}>
 			<Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
-				<Box cursor={"pointer"} fontSize={18}>
-					{/* {!isLiked ? <NotificationsLogo /> : <UnlikeLogo />} */}
-                    <UnlikeLogo/>
+				<Box onClick={handleLikePost} cursor={"pointer"} fontSize={18}>
+					{!isLiked ? <NotificationsLogo /> : <UnlikeLogo />}
+                    {/* <UnlikeLogo/> */}
 				</Box>
 
 				<Box cursor={"pointer"} fontSize={18} >
@@ -33,12 +34,12 @@ const PostFooter = ({ post, isProfilePage, creatorProfile }) => {
 				</Box>
 			</Flex>
 			<Text fontWeight={600} fontSize={"sm"}>
-				10 likes
+				{likes} likes
 			</Text>
 
 			{isProfilePage && (
 				<Text fontSize='12' color={"gray"}>
-					Posted 12s ago
+					Posted {timeAgo(post.createdAt)}
 				</Text>
 			)}
 
