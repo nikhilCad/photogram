8e03c0f445fe9d936d756c3c69869504be53e978
nikhilcad <nikhilcaddilac@gmail.com>
2024-02-03 18:26:47 +0530

    post modal is now checking for actual data instead of dummy text

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -18,49 +18,49 @@ import { AiFillHeart } from "react-icons/ai";
 import { FaComment } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import Comment from "../Comment/Comment";
-// import PostFooter from "../FeedPosts/PostFooter";
+import PostFooter from "../FeedPosts/PostFooter";
 import useUserProfileStore from "../../store/userProfileStore";
 import useAuthStore from "../../store/authStore";
-// import useShowToast from "../../hooks/useShowToast";
+import useShowToast from "../../hooks/useShowToast";
 import { useState } from "react";
-// import { deleteObject, ref } from "firebase/storage";
-// import { firestore, storage } from "../../firebase/firebase";
-// import { arrayRemove, deleteDoc, doc, updateDoc } from "firebase/firestore";
-// import usePostStore from "../../store/postStore";
+import { deleteObject, ref } from "firebase/storage";
+import { firestore, storage } from "../../firebase/firebase";
+import { arrayRemove, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import usePostStore from "../../store/postStore";
 // import Caption from "../Comment/Caption";
 
 const ProfilePost = ({ post }) => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const userProfile = useUserProfileStore((state) => state.userProfile);
 	const authUser = useAuthStore((state) => state.user);
-	// const showToast = useShowToast();
-	// const [isDeleting, setIsDeleting] = useState(false);
-	// const deletePost = usePostStore((state) => state.deletePost);
-	// const decrementPostsCount = useUserProfileStore((state) => state.deletePost);
+	const showToast = useShowToast();
+	const [isDeleting, setIsDeleting] = useState(false);
+	const deletePost = usePostStore((state) => state.deletePost);
+	const decrementPostsCount = useUserProfileStore((state) => state.deletePost);
 
-	// const handleDeletePost = async () => {
-	// 	if (!window.confirm("Are you sure you want to delete this post?")) return;
-	// 	if (isDeleting) return;
+	const handleDeletePost = async () => {
+		if (!window.confirm("Are you sure you want to delete this post?")) return;
+		if (isDeleting) return;
 
-	// 	try {
-	// 		const imageRef = ref(storage, `posts/${post.id}`);
-	// 		await deleteObject(imageRef);
-	// 		const userRef = doc(firestore, "users", authUser.uid);
-	// 		await deleteDoc(doc(firestore, "posts", post.id));
+		try {
+			const imageRef = ref(storage, `posts/${post.id}`);
+			await deleteObject(imageRef);
+			const userRef = doc(firestore, "users", authUser.uid);
+			await deleteDoc(doc(firestore, "posts", post.id));
 
-	// 		await updateDoc(userRef, {
-	// 			posts: arrayRemove(post.id),
-	// 		});
+			await updateDoc(userRef, {
+				posts: arrayRemove(post.id),
+			});
 
-	// 		deletePost(post.id);
-	// 		decrementPostsCount(post.id);
-	// 		showToast("Success", "Post deleted successfully", "success");
-	// 	} catch (error) {
-	// 		showToast("Error", error.message, "error");
-	// 	} finally {
-	// 		setIsDeleting(false);
-	// 	}
-	// };
+			deletePost(post.id);
+			decrementPostsCount(post.id);
+			showToast("Success", "Post deleted successfully", "success");
+		} catch (error) {
+			showToast("Error", error.message, "error");
+		} finally {
+			setIsDeleting(false);
+		}
+	};
 
 	return (
 		<>
@@ -150,8 +150,8 @@ const ProfilePost = ({ post }) => {
 											_hover={{ bg: "whiteAlpha.300", color: "red.600" }}
 											borderRadius={4}
 											p={1}
-											// onClick={handleDeletePost}
-											// isLoading={isDeleting}
+											onClick={handleDeletePost}
+											isLoading={isDeleting}
 										>
 											<MdDelete size={20} cursor='pointer' />
 										</Button>
@@ -162,9 +162,9 @@ const ProfilePost = ({ post }) => {
 								{/* Overflow gives scrollbar if too many comments */}
 								<VStack w='full' alignItems={"start"} maxH={"350px"} overflowY={"auto"}>
 									{/* {post.caption && <Caption post={post} />} */}
-									{/* {post.comments.map((comment) => (
+									{post.comments.map((comment) => (
 										<Comment key={comment.id} comment={comment} />
-									))} */}
+									))}
 									<Comment/>
 									<Comment/>
 									<Comment/>
@@ -176,7 +176,7 @@ const ProfilePost = ({ post }) => {
 								</VStack>
 								<Divider my={4} bg={"gray.8000"} />
 
-								{/* <PostFooter isProfilePage={true} post={post} /> */}
+								<PostFooter isProfilePage={true} post={post} />
 							</Flex>
 						</Flex>
 					</ModalBody>
